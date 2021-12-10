5b100a2fe064ef3b075faabaf02061a8b866846c
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-10 15:05:10 +0200

    Create Main.java

---
+++ Level_2/Term_1/Programming Techniqus/Sections/Code/sec_6/src/bicycle/Main.java
@@ -0,0 +1,9 @@
+package bicycle;
+
+public class Main {
+    public static void main(String[] args) {
+        MountBicycle m = new MountBicycle(10, 20, 78);
+
+        System.out.println("m = " + m);
+    }
+}
