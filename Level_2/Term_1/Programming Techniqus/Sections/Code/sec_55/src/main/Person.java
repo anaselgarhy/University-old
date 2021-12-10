bbc87dae10262101ed758547fcf5fc31194abf95
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-10 15:03:39 +0200

    Create Person.java

---
+++ Level_2/Term_1/Programming Techniqus/Sections/Code/sec_55/src/main/Person.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Person {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
