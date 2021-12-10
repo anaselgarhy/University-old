d05917152756689224b89e37cb1448f407c1fca6
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-10 14:59:56 +0200

    Create Main.java

---
+++ Level_2/Term_1/Programming Techniqus/Sections/Code/sec_4/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        // Set name
+        p.setName("Anas");
+
+        p.setId(998);
+
+        System.out.println(p);
+
+    }
+}
