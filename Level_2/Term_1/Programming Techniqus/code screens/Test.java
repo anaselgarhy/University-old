475f8846a4e95e702abb2716085ee2121272ecdc
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2021-12-10 14:59:12 +0200

    Create Test.java

---
+++ Level_2/Term_1/Programming Techniqus/code screens/Test.java
@@ -0,0 +1,9 @@
+// File name are Test.java
+public class Test {
+   public static void main(String[] args){
+      int i = 0;
+      for (i = 100; i <= 0; i -= 10){
+         System.out.print(i +", ");
+      }
+   }
+}  
