10e33351ae3d2a28e2ef1e1801286ee48d4dcfe2
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-01-07 16:35:17 +0200

    Complete operation enum

--- Level_2/Term_1/Programming Techniqus/Assignments/Assignment 3/simple-calc/src/main/java/com/anas/code/simplecalc/enums/OP.java
+++ Level_2/Term_1/Programming Techniqus/Assignments/Assignment 3/simple-calc/src/main/java/com/anas/code/simplecalc/enums/OP.java
@@ -1,8 +1,18 @@
 package com.anas.code.simplecalc.enums;
 
 public enum OP {
-    ADDITION,
-    SUBTRACTION,
-    MULTIPLICATION,
-    DIVISION
+    ADDITION('+'),
+    SUBTRACTION('-'),
+    MULTIPLICATION('*'),
+    DIVISION('/');
+
+    private final char symbol;
+
+    OP(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public char getSymbol() {
+        return this.symbol;
+    }
 }
